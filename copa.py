2c49b929b236c278031e9524f4d00333bd8ab657
sahmanuela <samantha.manuela@hotmail.com>
2022-11-21 09:52:09 -0300

    menu

--- copa.py
+++ copa.py
@@ -5,12 +5,15 @@ print('{:^40}'.format('COPA DO MUNDO'))
 print('-=' * 20)
 
 opcao = 0
-while opcao != 9:
-    print('-=' * 25)
-    opcao = int(input(f'MENU:\n[ 1 ] Sair do programa\n[ 2 ] Nova equipe\n[ 3 ] Novo jogo\n[ 4 ] Número total de jogos armazenados no “banco” da Copa do Mundo\n[ 5 ] Número total de equipes no “banco” da Copa do Mundo.\n[ 6 ] Gravar os dados dos jogos no arquivo:\n[ 7 ] Listar os jogos que constam no banco e suas respectivas equipes\n[ 8 ] Pesquisar por país\n[ 9 ] Apagar jogo\nInsira a sua opção: '))
-    print('-=' * 25)
+while opcao != 8:
+    print('--' * 20)
+    opcao = int(input(f'MENU:\n[ 1 ] Sair do programa\n[ 2 ] Nova equipe\n[ 3 ] Novo jogo\n[ 4 ] Número total de jogos armazenados no “banco” da Copa do Mundo\n[ 5 ] Número total de equipes no “banco” da Copa do Mundo.\n[ 6 ] Listar os jogos que constam no banco e suas respectivas equipes\n[ 7 ] Pesquisar por país\n[ 8 ] Apagar jogo\nInsira a sua opção: '))
+    print('--' * 20)
 
     if opcao == 1:
+        time.sleep(1)
+        print('Você está saindo do programa...')
+        time.sleep(1)
         break
 
     elif opcao == 2:
@@ -35,8 +38,7 @@ while opcao != 9:
     elif opcao == 8:
         break
 
-    elif opcao == 9:
-        break
-
     else:
+        time.sleep(1)
         print('Opção inválida! Tente novamente.')
+        time.sleep(1)
