4fe74c5e5d571135a8c31693cc31142893851910
sahmanuela <samantha.manuela@hotmail.com>
2022-11-21 09:44:12 -0300

    menu

--- copa.py
+++ copa.py
@@ -1,3 +1,42 @@
+import time
+
 print('-=' * 20)
 print('{:^40}'.format('COPA DO MUNDO'))
 print('-=' * 20)
+
+opcao = 0
+while opcao != 9:
+    print('-=' * 25)
+    opcao = int(input(f'MENU:\n[ 1 ] Sair do programa\n[ 2 ] Nova equipe\n[ 3 ] Novo jogo\n[ 4 ] Número total de jogos armazenados no “banco” da Copa do Mundo\n[ 5 ] Número total de equipes no “banco” da Copa do Mundo.\n[ 6 ] Gravar os dados dos jogos no arquivo:\n[ 7 ] Listar os jogos que constam no banco e suas respectivas equipes\n[ 8 ] Pesquisar por país\n[ 9 ] Apagar jogo\nInsira a sua opção: '))
+    print('-=' * 25)
+
+    if opcao == 1:
+        break
+
+    elif opcao == 2:
+        break
+        
+
+    elif opcao == 3:
+        break
+
+    elif opcao == 4:
+        break
+
+    elif opcao == 5:
+        break
+    
+    elif opcao == 6:
+        break
+    
+    elif opcao == 7:
+        break
+
+    elif opcao == 8:
+        break
+
+    elif opcao == 9:
+        break
+
+    else:
+        print('Opção inválida! Tente novamente.')
