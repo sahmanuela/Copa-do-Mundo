c7cc5c7268cc8d7ae48855e0173c7207d5d4db25
sahmanuela <samantha.manuela@hotmail.com>
2022-11-26 09:27:31 -0300

    nova equipe

--- copa.py
+++ copa.py
@@ -1,18 +1,55 @@
 import time
+import json
+import os
 
 print('-=' * 20)
 print('{:^40}'.format('COPA DO MUNDO'))
 print('-=' * 20)
 
-def novaEquipe(pais, abreviacao, grupo):
-    while True:
-        pais = input('Insira o país: ')
-        abreviacao = input('Insira a abreviação do país: ')
-        grupo = input('Insira o grupo do país: ')
-        
+if os.path.exists('times.json'):
+  times_file = open('times.json', 'r+')  
+  if len(times_file.readline()) > 0:
+    times_file.seek(0)
+    times = json.load(times_file)
+  else:
+    times = list()  
+else:
+  times_file = open('times.json', 'w')  
+  times = list()
 
-def novoJogo(dadosEquipe, gols, faltas ):
-    quit()
+
+def salvar_times_file():
+  times_file.truncate(0)
+  times_file.seek(0)
+  times_file.write(json.dumps(times))  
+
+
+def novoTime():
+    salvar_times = False
+    while not salvar_times:
+        pais = input('Insira o país da seleção: ')
+        abreviacao = input('Insira a abreviação do país: ')
+        add_grupo = False
+        while not add_grupo:
+            grupo = input('Insira o grupo do país: ').upper()
+            if len([time for time in times if time.get('Grupo') == grupo]) == 4:
+                print('Este grupo já possui 4 seleções!\nEscolha outro grupo.')
+            else: 
+                add_grupo = True
+            times.append({'País': pais, 'Abreviação': abreviacao ,'Grupo': grupo})
+        if input('Cadastrar outra seleção(S/N): ').upper != 'S':
+            salvar_times = True
+        salvar_times_file()
+
+def listarTimes(times):
+  if len(times) == 0:
+    print("Não há seleções cadastradas")
+  for time in times:
+    print(f"Seleção: {time.get('País')} - Grupo: {time.get('Grupo')}")
+
+def listarGrupo():
+  grupo = input('Grupo: ').upper() 
+  listarTimes([time for time in times if time.get('Grupo') == grupo])
 
 opcao = 0
 while opcao != 8:
@@ -27,11 +64,8 @@ while opcao != 8:
         break
 
     elif opcao == 2:
-        with open ("nova-equipe.txt", "a+") as arquivo:
-            for linha in arquivo.readlines():
-                print(linha)
-        
-
+        novoTime()
+    
     elif opcao == 3:
         break
 
